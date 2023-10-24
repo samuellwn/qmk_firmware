050fcc746fa2fd5f734e910638353b23797fe76a
Samuel Loewen <samuellwn@gmail.com>
2023-10-24 18:04:29 -0400

    keymap tweak

--- keyboards/moonlander/keymaps/samuellwn/keymap.c
+++ keyboards/moonlander/keymaps/samuellwn/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_EQL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_HYPR,           KC_MEH,  KC_H,    KC_J,    KC_K,    KC_L,    LT(MDIA, KC_SCLN),KC_QUOT,
         KC_LSFT, LALT_T(KC_Z),KC_X,KC_C,    KC_V,    KC_B,                                KC_N,    KC_M,    KC_COMM, KC_DOT,  RGUI_T(KC_SLSH), KC_RSFT,
     LT(SYMB,KC_GRV),WEBUSB_PAIR,A(KC_LSFT),KC_LEFT, KC_RGHT,  KC_BSPC,                  KC_ESC,    KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, MO(SYMB),
-                                      LSFT_T(KC_TAB),KC_LGUI, KC_LCTL,           RALT_T(KC_ENT), KC_RCTL,RSFT_T(KC_SPC)
+                                      LSFT_T(KC_TAB),KC_LGUI,LCTL_T(KC_BSPC),    RALT_T(KC_ENT), KC_RCTL,RSFT_T(KC_SPC)
     ),
 
     [SYMB] = LAYOUT_moonlander(
