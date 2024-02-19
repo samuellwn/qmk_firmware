e0228c4401fab0125cb9fdec97305b1dae46f28b
Samuel Loewen <samuellwn@gmail.com>
2024-02-19 17:56:56 -0500

    compat keymap layer and keys colored by layer

--- keyboards/moonlander/keymaps/samuellwn/keymap.c
+++ keyboards/moonlander/keymaps/samuellwn/keymap.c
@@ -22,16 +22,23 @@
 #include "version.h"
 
 enum layers {
-    BASE,  // default layer
-    THMOD, // thumb modifiers
-    THKEY, // thumb keys
-    SYMB,  // symbols
-    MDIA,  // media keys
+    BASE,   // default layer
+    COMPAT, // default layer for things that don't play well with fancy stuff
+    THMOD,  // thumb modifiers
+    THKEY,  // thumb keys
+    SYMB,   // symbols
+    MDIA,   // media keys
+    LAYER_COUNT
 };
 
-//const rgblight_segment_t PROGMEM rgb_layer_thmod[] = RGBLIGHT_LAYER_SEGMENTS(
-//
-//);
+const uint8_t PROGMEM layer_colors[LAYER_COUNT] = {
+    [BASE] = 85,
+    [COMPAT] = 0,
+    [THMOD] = 43,
+    [THKEY] = 180,
+    [SYMB] = 169,
+    [MDIA] = 169
+};
 
 enum custom_keycodes {
     VRSN = SAFE_RANGE,
@@ -40,12 +47,30 @@ enum custom_keycodes {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_moonlander(
-        KC_DEL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LEFT,           KC_RGHT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+        KC_DEL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    TG(COMPAT),     TG(COMPAT), KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
         KC_BSPC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    TG(SYMB),         TG(SYMB), KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
-        KC_EQL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_HYPR,           KC_MEH,  KC_H,    KC_J,    KC_K,    KC_L,    LT(MDIA, KC_SCLN),KC_QUOT,
+        KC_EQL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    TG(MDIA),         TG(MDIA), KC_H,    KC_J,    KC_K,    KC_L,    LT(MDIA, KC_SCLN),KC_QUOT,
         CW_TOGG, LALT_T(KC_Z),KC_X,KC_C,    KC_V,    KC_B,                                KC_N,    KC_M,    KC_COMM, KC_DOT,  RGUI_T(KC_SLSH), KC_BSLS,
         _______,WEBUSB_PAIR,A(KC_LSFT),KC_LEFT,LT(SYMB,KC_RGHT),  TG(THMOD), TG(THKEY),LT(SYMB,KC_UP),KC_DOWN, KC_LBRC, KC_RBRC, MO(SYMB),
                                       LSFT_T(KC_TAB),KC_LGUI,LCTL_T(KC_ESC), RALT_T(KC_ENT),LCTL_T(KC_BSPC),RSFT_T(KC_SPC)
+    ),
+
+    [COMPAT] = LAYOUT_moonlander(
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______,           _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    TG(SYMB),         TG(SYMB), KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    TG(MDIA),         TG(MDIA), KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+        KC_LCTL, KC_GRV,  _______, KC_LEFT, KC_RGHT,          TG(THMOD),       TG(THKEY),          KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, KC_DEL,
+                                            KC_SPC,  KC_LSFT, KC_LCTL,           KC_LALT, KC_LSFT, KC_SPC
+    ),
+
+    [SYMB] = LAYOUT_moonlander(
+        VRSN,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,           _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+        _______, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, _______,           _______, KC_UP,   KC_7,    KC_8,    KC_9,    KC_ASTR, KC_F12,
+        _______, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,  _______,           _______, KC_DOWN, KC_4,    KC_5,    KC_6,    KC_PLUS, _______,
+        _______, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD,                             KC_AMPR, KC_1,    KC_2,    KC_3,    KC_BSLS, _______,
+        _______, _______, _______, _______, _______,          _______,           _______,          _______, KC_0,    KC_DOT,  KC_EQL,  EE_CLR,
+                                            _______, _______, _______,           _______, _______, _______
     ),
 
     [THKEY] = LAYOUT_moonlander(
@@ -54,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______,           _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______,                             _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______,          _______,           _______,          _______, _______, _______, _______, _______,
-                                            KC_TAB,  _______, KC_ESC,            KC_ENT,  KC_BSPC, KC_SPC
+                                            KC_TAB,  KC_LGUI, KC_ESC,            KC_ENT,  KC_BSPC, KC_SPC
     ),
 
     [THMOD] = LAYOUT_moonlander(
@@ -64,15 +89,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______,                             _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______,          _______,           _______,          _______, _______, _______, _______, _______,
                                             KC_LSFT, KC_LGUI, KC_LCTL,           KC_RALT, KC_LCTL, KC_RSFT
-    ),
-
-    [SYMB] = LAYOUT_moonlander(
-        VRSN,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,           _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-        _______, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, _______,           _______, KC_UP,   KC_7,    KC_8,    KC_9,    KC_ASTR, KC_F12,
-        _______, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,  _______,           _______, KC_DOWN, KC_4,    KC_5,    KC_6,    KC_PLUS, _______,
-        _______, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD,                             KC_AMPR, KC_1,    KC_2,    KC_3,    KC_BSLS, _______,
-        EE_CLR,  _______, _______, _______, _______,          _______,           _______,          _______, KC_0,    KC_DOT,  KC_EQL,  _______,
-                                            _______, _______, _______,           _______, _______, _______
     ),
 
     [MDIA] = LAYOUT_moonlander(
@@ -105,6 +121,23 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
     return true;
 }
+
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
+        for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
+            uint8_t index = g_led_config.matrix_co[row][col];
+            if (index >= led_min && index < led_max && index != NO_LED) {
+                uint8_t layer = layer_switch_get_layer((keypos_t){col,row});
+                RGB rgb = hsv_to_rgb((HSV){pgm_read_byte(&layer_colors[layer]), 255, 100});
+                rgb_matrix_set_color(index, rgb.r, rgb.g, rgb.b);
+            }
+        }
+    }
+
+    return false;
+}
+
+
 
 /* default keymap
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
